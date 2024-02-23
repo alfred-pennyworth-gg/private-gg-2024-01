0d21a4c94bdf51e1075585d01b4a2c40da6997f7
Alfred Pennyworth <alfred-pennyworth-gg@protonmail.com>
2024-02-23 10:33:21 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -2,7 +2,19 @@
 
 import antigravity
 
-SEED=49a0f18b-ecab-4517-b567-5628cdcc6222
+CERT="""-----BEGIN DSA PRIVATE KEY-----
+MIIBuwIBAAKBgQCQ0efhZ78iz8IM8dh7W4BdVhEgRcFlrB0NmFeNd4NwzKcVwvRp
+9ERGXuqBdoMDsx3pIA+sfyjtPJQhhjCmldC0TC25zAqAqVpxkhayaRV077THCzzD
+wWEvLBTbAldXPfHYYAKbjGc+ATc9ja+1CqbaMb4L6lCxVmTdn8gmmrr2WwIVAK01
+MY6BbJ6B2Uo7Eiuzgu0wXss7AoGAAQl6TonsdnUkfcCCgyO6hgmC/Lp9SmSbu2Af
++NagpFkwdu+ce4apB6ofdcDsfOLVn8HIOMEG4JXsPz4Q3adR2MIkuEr+Mqq6bY41
+YrjqNlmUdyub9EnhodxrEfciglBXWUHG4au42nRI21U6GNRYHzy7XBZBK09f69HY
++WorFGACgYEAjRohuVzCtt5bgeqAi3bS2cebIVk6JI7NWwY0RI6PqbiJn9/1mvJI
+HcmUQadSJ7uBEfBoeDCWVaqdSco4WY6IN9+hk4J6Hk2pLgYtDlERPtdL+dCWtNbb
+pI9GyQVyp34Y3vKsdkdZuiWPWT3PgJN1caV4rzJ41v71KSMkMVxIk1sCFGNuiD6j
+xHLK2kvQP/SeXz8BSiQ1
+-----END DSA PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
